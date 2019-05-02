8cff686e90558601a52821e28e89762e0e526478
Danzed11 <49248607+Danzed11@users.noreply.github.com>
2019-05-02 13:07:11 -0600

    firstWaldo assignment

---
+++ waldo.js
@@ -0,0 +1,16 @@
+
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i);   // execute callback
+    }
+  }
+}
+
+function actionWhenFound(indexNumber) {
+ 
+  console.log("Found him at index " + indexNumber);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
