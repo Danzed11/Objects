8c2205433a7fe5bf4d4fd11871f5fceb8dff279e
Danzed11 <49248607+Danzed11@users.noreply.github.com>
2019-05-02 13:33:11 -0600

    forEachWaldo
    adding forEach

--- waldo.js
+++ waldo.js
@@ -1,10 +1,10 @@
 
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-    }
+arr.forEach(function(waldo, index) {
+  if (waldo === "Waldo") {
+      found(index);   // execute callback
   }
+  });
 }
 
 function actionWhenFound(indexNumber) {
